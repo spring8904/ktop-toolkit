c15fa7f668a03b411e0bf05f7bab6bf286c840f2
spring8904 <lamthang890123music@gmail.com>
2024-08-12 15:52:34 +0700

    refactor: Refactor rounding function in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const $ = document.querySelector.bind(document)
 
-const lamTron = (num) => Math.round(num * 100) / 100
+const round = (num) => Math.round(num * 100) / 100
 
 const result = $('#result')
 const btnCopy = $('#btn-copy')
@@ -14,30 +14,24 @@ const formCream = $('#form-cream')
 
 formCream.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const data = formCream.querySelector('input').value.trim()
-
-  result.value = lamTron((data - 300) / 1500)
+  result.value = round((data - 300) / 1500)
 })
 
 const formCream2 = $('#form-cream2')
 
 formCream2.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const data = formCream2.querySelector('input').value.trim()
-
-  result.value = lamTron((data - 200) / 1500)
+  result.value = round((data - 200) / 1500)
 })
 
 const formCream3 = $('#form-cream3')
 
 formCream3.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const data = formCream3.querySelector('input').value.trim()
-
-  result.value = lamTron((data - 500) / 1500)
+  result.value = round((data - 500) / 1500)
 })
 
 const result2 = $('#result2')
@@ -52,18 +46,14 @@ const formCoke = $('#form-coke')
 
 formCoke.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const data = formCoke.querySelector('input').value.trim()
-
-  result2.value = lamTron((data - 1) * 0.8)
+  result2.value = round((data - 1) * 0.8)
 })
 
 const formSprite = $('#form-sprite')
 
 formSprite.addEventListener('submit', (event) => {
   event.preventDefault()
-
   const data = formSprite.querySelector('input').value.trim()
-
-  result2.value = lamTron((data - 0.8) * 0.8)
+  result2.value = round((data - 0.9) * 0.8)
 })
